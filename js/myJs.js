0e001f8f9df87a3572de90f0fc674979c42434bb
Dương Văn Sơn <sondv@ngvgroup.vn>
2022-06-03 16:29:27 +0700

    1

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "He luu bạn Hiền!",
+  text2: "Mình có điều này muốn hỏi bạn nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Bạn yêu tớ có phải không nào ._.",
+  text4: "Nếu bạn ko trả lời mà thoát ra tức là muốn làm vợ mình rùi đó nha :v",
+  text5: "Bạn mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do bạn thích mình đi :vvvv",
+  text8: "Gửi cho mình <3",
+  text9: "Vì mình đẹp try vlllll",
+  text10: "Mình biết mà ^^ Yêu bạn 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay mình qua đón bạn đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho mình đi nàooo",
   text12: "Okii lunn <3",
 };
 
